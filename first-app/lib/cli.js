865b3fe2d287040e97a4cc28f3bc1fc88ac24f60
greyhands2 <greyhands2@gmail.com>
2024-03-20 11:14:18 +0100

    added cli command data for command stats

--- first-app/lib/cli.js
+++ first-app/lib/cli.js
@@ -7,6 +7,10 @@ const readline = require('readline')
 
 const util = require('util') // to set up cli specific login
 
+const os = require('os') // access things like load average, number of cpus, free memory and uptime
+
+const v8 = require('v8') // get things like malloced_memory, peak_malloced_memory, allocated heap used, total heap allocated
+
 const debug = util.debuglog('cli')
 
 const events = require('events')
@@ -81,8 +85,92 @@ cli.responders = {}
 // help / Man
 
 cli.responders.help = function(){
-	console.log("You asked for help")
+	const commands = {
+		'exit': 'Kill the CLI and the rest of the application',
+		'man': 'Show this help page',
+		'help': 'Alias of the "man" command',
+		
+		'stats': 'Get statistics on the underlying operating system and resource utilization',
+		'list users': 'Show a list of all the registered (undeleted) users in the system',
+		'more user info --{userId}': 'Show details of a specific user',
+		'list checks --up --down': 'Show a list of all the active checks in the system including their state. The "--up" and "--down" flags are both optional',
+		'more check info --{checkId}': 'Show details of a specified check',
+		'list logs': 'Show a list of all the log files available to be read (compressed and uncompressed)',
+		'more log info --{fileName}': 'Show details of a specified log file',
+	} 
+
+	// show a header for the help page that is as wide as the screem
+
+	cli.horisontalLine()
+	cli.centered("CLI MANUAL")
+	cli.horisontalLine()
+	cli.verticalSpace(2)
+
+	// ahow each command followed by it's explanation in white and yellow respectively
+	for(let key in commands){
+		if(commands.hasOwnProperty(key)){
+			let value = commands[key]
+			let line = '\x1b[33m'+key+'\x1b[0m'
+			let padding = 60 - line.length
+
+			for(let i = 0; i < padding; i++){
+				line+=' '
+			}
+			line+=value
+			console.log(line)
+			cli.verticalSpace()
+		}
+	}
+
+	cli.verticalSpace(1)
+
+	// end with another horizontal line
+	cli.horisontalLine()
 }
+
+
+// create a vertical space
+cli.verticalSpace = function(lines){
+	lines = typeof(lines) == 'number' && lines > 0 ? lines : 1
+	for(let i = 0; i < lines; i++){
+		console.log('')
+	}
+}
+
+
+cli.horisontalLine = function(){
+	// get available screen size
+	let width = process.stdout.columns
+
+	let line = ''
+	for(let i = 0; i < width; i++){
+		line+='-'
+	}
+	console.log(line)
+}
+
+// create centered text on the screen
+cli.centered = function(str){
+	str = typeof(str) == 'string' && str.trim().length > 0 ? str.trim() : ''
+
+	// get the available screen size
+	let width = process.stdout.columns
+
+	//calculate the left padding there should be
+	let leftPadding = Math.floor( (width - str.length) / 2 )
+
+	// put in left padded spaces before the string itself
+	let line = ''
+
+	for(let i = 0; i < leftPadding; i++){
+		line+=' '
+	}
+
+	line+= str
+	console.log(line)
+
+}
+
 
 cli.responders.exit = function(){
 	
@@ -90,7 +178,45 @@ cli.responders.exit = function(){
 }
 
 cli.responders.stats = function(){
-	console.log("You asked for stats")
+	// compile an object of stats
+	const stats = {
+		'Load Average': os.loadavg().join(' '), // loadavg() returns an array so we join it to make is a set of strings
+		'CPU Count': os.cpus().length, // returns an array cpus and since we just want to know how many cpus we get a length of the array
+		'Free Memory': os.freemem(),
+		'Current Malloced Memory': v8.getHeapStatistics().malloced_memory,
+		'Peak Malloced Memory': v8.getHeapStatistics().peak_malloced_memory,
+		'Allocated Heap Used (%)': Math.round((v8.getHeapStatistics().used_heap_size / v8.getHeapStatistics().total_heap_size) * 100 ),
+		'Available Heap Allocated (%)': Math.round((v8.getHeapStatistics().total_heap_size / v8.getHeapStatistics().heap_size_limit) * 100 ),
+		'Uptime': os.uptime()+' Seconds'
+
+	}	
+
+	//Create a header for the stats
+	cli.horisontalLine()
+	cli.centered("SYSTEM STATISTICS")
+	cli.horisontalLine()
+	cli.verticalSpace(2)
+
+	// log out eaach stat
+	for(let key in stats){
+		if(stats.hasOwnProperty(key)){
+			let value = stats[key]
+			let line = '\x1b[33m'+key+'\x1b[0m'
+			let padding = 60 - line.length
+
+			for(let i = 0; i < padding; i++){
+				line+=' '
+			}
+			line+=value
+			console.log(line)
+			cli.verticalSpace()
+		}
+	}
+
+	cli.verticalSpace(1)
+
+	// end with another horizontal line
+	cli.horisontalLine()
 }
 
 cli.responders.listUsers = function(){
